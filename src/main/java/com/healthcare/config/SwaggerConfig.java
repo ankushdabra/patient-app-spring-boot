2841d9117e52f42130e22b696dd1b32a6c836444
Ankush Dabra <ankush.singh.dabra@gmail.com>
2026-02-10 22:16:20 +0530

    CORS implemented

--- src/main/java/com/healthcare/config/SwaggerConfig.java
+++ src/main/java/com/healthcare/config/SwaggerConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -18,6 +21,10 @@ public class SwaggerConfig {
                 .title("Healthcare API")
                 .description("Healthcare Management APIs")
                 .version("1.0"))
+            .servers(List.of(
+                new Server().url("https://patient-app-spring-boot-production.up.railway.app").description("Production Server"),
+                new Server().url("http://localhost:8080").description("Local Server")
+            ))
             .addSecurityItem(
                 new SecurityRequirement().addList("bearerAuth")
             )
